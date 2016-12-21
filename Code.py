5733030f88586b6e3ecc6bbf12d2ef657a227c5f
KyleCow <9131@kingdown.wilts.sch.uk>
2016-12-21 15:26:06 +0000

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,9 @@
+m = 1
+import math
+while 1 == 1:
+    m = m + 1
+    x =(m*(m+1))/2
+    if float(x).is_integer():
+        s = math.sqrt(x)
+        if float(s).is_integer():
+            print("SquareTriangle=",x)
